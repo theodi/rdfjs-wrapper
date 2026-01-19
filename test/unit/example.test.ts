55ddf650c5c439ec3d9cccb1d6304ce15d22ad40
Matthieu Bosquet <matthieubosquet@gmail.com>
2026-01-19 18:40:43 +0000

    Fix test with imports

--- test/unit/example.test.ts
+++ test/unit/example.test.ts
@@ -1,10 +1,14 @@
 import assert from "node:assert";
 import { describe, it } from "node:test";
+import { Vocabulary } from "./Vocabulary.js"
 
 import { example } from "rdfjs-wrapper";
 
 describe("Example", async () => {
     it("returns x", () => {
         assert.equal(example(), "x");
+    })
+    it("returns child", () => {
+        assert.equal(Vocabulary.hasChild.value, "https://example.org/hasChild");
     })
 })
